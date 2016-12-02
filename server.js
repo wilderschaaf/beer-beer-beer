1b956a8fff16f9b9158d64643d7321c00a5059b0
Wilder Schaaf <wilderschaaf@gmail.com>
2016-12-02 14:31:38 -0800

    fixed scraper

--- server.js
+++ server.js
@@ -189,25 +189,25 @@ app.get('/', function(req, res){
 
 })
 
-app.get('/bsearch/', function(req, res){
-	console.log('beer: ' + req.query['beer'])
-	//res.sendFile(__dirname + "/public/home.html")
-	db.any("select * from calibeers where lower(brewery) like lower(${term}) or lower(beername) like lower(${term})", {term: '%' + req.query['beer'] + '%'})
-		.then( function (data){
-			console.log(data)
-			res.render('home', {
-				beers: data
-			})
-		})
-		.catch( function (error){
-			console.error('search error:', error)
-			res.render('home', {
-				beer: "Error fetching results from database"
-			})
-		})
+// app.get('/bsearch/', function(req, res){
+// 	console.log('beer: ' + req.query['beer'])
+// 	//res.sendFile(__dirname + "/public/home.html")
+// 	db.any("select * from calibeers where lower(brewery) like lower(${term}) or lower(beername) like lower(${term})", {term: '%' + req.query['beer'] + '%'})
+// 		.then( function (data){
+// 			console.log(data)
+// 			res.render('home', {
+// 				beers: data
+// 			})
+// 		})
+// 		.catch( function (error){
+// 			console.error('search error:', error)
+// 			res.render('home', {
+// 				beer: "Error fetching results from database"
+// 			})
+// 		})
 
 	
-})
+// })
 
 
 
