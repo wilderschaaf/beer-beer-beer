2ebe731f87d0d413a06bb0f091cc569c6146a46c
Wilder Schaaf <wilderschaaf@gmail.com>
2016-12-02 14:54:42 -0800

    fixed scraper

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ function mycb(bl, callback){
 					queries = []
 					i = 3
 					while (data.eq(i).children()[0] != undefined){
-						 if (!Number.isNaN(parseFloat(data.eq(i).children().eq(3).text())) && parseFloat(data.eq(i).children().eq(4).text()) > 2){
+						 if (!Number.isNaN(parseFloat(data.eq(i).children().eq(3).text())) && parseNumRatings(data.eq(i).children().eq(4).text()) > 2){
 							queries.push(t.none("INSERT INTO calibeers (brewery, beername, style, abv, avgrating, numratings, brorating) VALUES ($1, $2, $3, $4, $5, $6, $7)", 
 								[brewery, data.eq(i).children().eq(0).text(), 
 								data.eq(i).children().eq(1).text(), 
