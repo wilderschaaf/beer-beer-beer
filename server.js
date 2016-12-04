d766a489522ef6dc6b2088f61cd7c61c4110a6e6
Wilder Schaaf <wilderschaaf@gmail.com>
2016-12-03 22:11:43 -0800

    full data display

--- server.js
+++ server.js
@@ -193,7 +193,7 @@ app.get('/', function(req, res){
 app.get('/bsearch/', function(req, res){
 	console.log('beer: ' + req.query['beer'])
 	//res.sendFile(__dirname + "/public/home.html")
-	db.any("select * from calibeers where lower(brewery) like lower(${term}) or lower(beername) like lower(${term})", {term: '%' + req.query['beer'] + '%'})
+	db.any("select * from calibeers where lower(brewery) like lower(${term}) or lower(beername) like lower(${term}) order by avgrating desc", {term: '%' + req.query['beer'] + '%'})
 		.then( function (data){
 			console.log(data)
 			res.render('home', {
