45de1e0e97c1ba9e2765f2b40eb6b4552df4bb14
Wilder Schaaf <wilderschaaf@gmail.com>
2017-01-08 17:14:04 -0800

    doing beer page

--- server.js
+++ server.js
@@ -386,7 +386,7 @@ app.get('/bsearch', function(req, res){
 })
 
 app.get('/beer/*', function(req, res){
-	console.log("here's the stuff "+ req.query['id'])
+	console.log("here's the stuff "+ req.query['beerid'])
 
 })
 
