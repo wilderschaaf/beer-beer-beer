58d97852fc7db0f155a7382f90ede0a23b4e91b0
Wilder Schaaf <wilderschaaf@gmail.com>
2017-01-18 09:32:05 -0800

    automating states

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ var db = pgp(conString)
 
 //scraping in VT data
 function usecallback(callback){
-	var state = 'CO'
-	var top = 340
+	var state = 'CT'
+	var top = 80
 	var brewlinks = []
 
 	var j 
@@ -268,7 +268,7 @@ function mycb(bl, callback){
 	var data
     var brewery
     var beerlink
-    var state = 'Colorado'
+    var state = 'Connecticut'
     var $
     var i 
     var queries
