d87df1c6d1ec1bc01ec0bb2723627fa74114c837
Wilder Schaaf <wilderschaaf@gmail.com>
2018-07-13 08:15:53 -0700

    nope

--- server.js
+++ server.js
@@ -303,7 +303,7 @@ function mycb(bl, callback){
 					data = $('#ba-content').find("table").eq(1).children()
 					queries = []
 					i = 3
-					console.log($('table.sortable').eq(0).children().children())
+					console.log($('table.sortable').eq(0).children().children().eq(0))
 					//console.log(data.eq(i).children()[0])
 					
 					while (data.eq(i).children()[0] != undefined){
