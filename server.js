9d16af5fa5d3cb585282e4f0e4d1340d7cb57954
Wilder Schaaf <wilderschaaf@gmail.com>
2018-07-25 09:37:54 -0700

    filtering by style

--- server.js
+++ server.js
@@ -514,10 +514,10 @@ function changeDescs(obbiej, dict){
 app.get('/beer/[0-9]*', function(req, res){
 	console.log("here's the stuff "+ req.query['beerid'])
 	console.log("and " + req.query['stid'])
-	var quer = (req.query['stid'] == 'undefined' || req.query['stid'] == 'US' || req.query['stid'] == '') ? "create or replace view testview as select brewery, beername, beerid, style, avgrating, state, (getSDistance(grabArray($(id)), desclist)) as distance, desclist from calibeers where beerid != $(id)":"create or replace view testview as select brewery, beername, beerid, style, avgrating, state, (getSDistance(grabArray($(id)), desclist)) as distance, desclist from calibeers where state=$(stid) and beerid != $(id)"
+	var quer = (req.query['stid'] == 'undefined' || req.query['stid'] == 'US' || req.query['stid'] == '') ? "create or replace view testview as select brewery, beername, beerid, style, avgrating, state, (getSDistance(grabArray($(id)), desclist)) as distance, desclist from calibeers where beerid != $(id) and style = $(style)":"create or replace view testview as select brewery, beername, beerid, style, avgrating, state, (getSDistance(grabArray($(id)), desclist)) as distance, desclist from calibeers where state=$(stid) and beerid != $(id) and style = $(style)"
 	db.one("select * from calibeers where beerid=$(id)", {id: req.query['beerid']})
 		.then( function (data){
-			db.none(quer, {id: req.query['beerid'], stid: req.query['stid']})
+			db.none(quer, {id: req.query['beerid'], stid: req.query['stid'], style: req.query['style']})
 				.then( function (data2){
 					db.many("select * from testview order by distance limit 5 offset 0")
 						.then( function (data3){
